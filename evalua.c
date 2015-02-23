eaa529a2936fc7fef5bd9ac698cb33de85866598
choyos <cesar91hoyos@gmail.com>
2015-02-23 20:47:39 +0100

    Creada función evalua

---
+++ evalua.c
@@ -0,0 +1,28 @@
+/*  Proyecto fin de carrera*/
+/* Optimizacion del stock de farmacos en hospitales*/
+/* Fecha: 2014-2015*/
+/*Apellidos: Hoyos Martín
+Nombre: César*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "typedef.h"
+#include <time.h>
+
+
+#define LUNES 0
+#define MARTES 1
+#define MIERCOLES 2
+#define JUEVES 3
+#define VIERNES 4
+#define SABADO 5
+#define DOMINGO 6
+
+int bisiesto(int year);
+//void obtenerMatrix(int filasMatrix,int TAM,int *matrixResult[][TAM], int *matrix1[filasMatrix][TAM], int numPedidos, int *vTamPedidos[]);
+
+float evalua(int argc, char *argv[]){
+		
+	return J;
+}
